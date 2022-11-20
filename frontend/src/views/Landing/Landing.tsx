d38c16131570143348e5c52cd10a66baf7392191
bnkc <levostatnigrosh@gmail.com>
2022-11-20 16:25:06 -0500

    sadf

--- frontend/src/views/Landing/Landing.tsx
+++ frontend/src/views/Landing/Landing.tsx
@@ -69,7 +69,7 @@ const Landing: React.FC = () => {
         setDisabled(true);
         // const url = new WebSocket(`ws://localhost:8000/api/v1/handles/${values.username}`);
         //now use the render url
-        const url = new WebSocket(`wss://www.handlefinder.com:10000/api/v1/handles/${values.username}`);
+        const url = new WebSocket(`wss://www.handlefinder.com/api/v1/handles/${values.username}`);
 
 
 
