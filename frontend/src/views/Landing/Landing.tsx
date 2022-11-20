9f54267d5f2ddaf110b9ee6008cbcc9c9a196dbb
bnkc <levostatnigrosh@gmail.com>
2022-11-20 15:31:11 -0500

    test

--- frontend/src/views/Landing/Landing.tsx
+++ frontend/src/views/Landing/Landing.tsx
@@ -69,7 +69,10 @@ const Landing: React.FC = () => {
         setDisabled(true);
         // const url = new WebSocket(`ws://localhost:8000/api/v1/handles/${values.username}`);
         //now use the render url
-        const url = new WebSocket(`wss://handlefinder-backend:10000/api/v1/handles/${values.username}`);
+        const url = new WebSocket(`ws://www.handlefinder.com:10000/api/v1/handles/${values.username}`);
+
+
+
         // now use wss for the www.handlefinder.com for the socket connection
         // const url = new WebSocket(`ws://handlefinder-backend:10000/api/v1/handles/${values.username}`);
 
